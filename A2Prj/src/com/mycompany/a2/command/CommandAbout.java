6bf180bef4b92f48c08482c3720d7cffd218139d
JorgeTadeo <jorge_tadeo@icloud.com>
2019-06-26 16:55:50 -0700

    added javadoc for CommandAbout.java

--- A2Prj/src/com/mycompany/a2/command/CommandAbout.java
+++ A2Prj/src/com/mycompany/a2/command/CommandAbout.java
@@ -3,19 +3,30 @@ package com.mycompany.a2.command;
 import com.codename1.ui.Command;
 import com.codename1.ui.events.ActionEvent;
 import com.mycompany.a2.game.IGameWorld;
-
+/*
+ * CommandAbout class is responsible to activate the command command from UI.
+ */
 public class CommandAbout extends Command {
 
-	
+	/**
+	 * Description: Gameworld about the game
+	 */
 	private IGameWorld gw;
 
-	
+	/**
+	 * Description : Constructor that links gameworld to about command.
+	 * @param gw : Input gameworld 
+	 * 
+	 */
 	public CommandAbout(IGameWorld gw) {
 		super("About");
 		this.gw = gw;
 	}
 	
-
+	/**
+	 * Description : Show about page.
+	 * 
+	 */
 	public void actionPerformed(ActionEvent e) {
 		//TODO
 	}
